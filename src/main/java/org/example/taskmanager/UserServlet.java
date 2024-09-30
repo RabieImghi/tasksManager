d7a7dd8a5424e6b952b640f3115a0bddd416670a
RabieImghi <rabieaitimghi7@gmail.com>
2024-09-30 20:26:22 +0100

    add jboss server for run application

---
+++ src/main/java/org/example/taskmanager/UserServlet.java
@@ -0,0 +1,29 @@
+package org.example.taskmanager;
+
+import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "UserServlet", value = "/user-servlet")
+public class UserServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello User!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
